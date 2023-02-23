9154a35fba7431e3e52682e677415c34d4c352f1
Angelo <amchengcuenca@gmail.com>
2023-02-22 18:41:13 -0800

    adds sfx for choosing a participant

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,101 +1,20 @@
-
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   cohort: { type: Number, required: true },
   participation: { type: Number, default: 0 },
-  isAdmin: { type: Boolean, default: false }
-})
+  isAdmin: { type: Boolean, default: false },
+});
 
 UserSchema.pre('save', async function (next) {
   const saltRounds = 10;
   this.password = await bcrypt.hash(this.password, saltRounds);
-  return next()
-})
-
-
+  return next();
+});
 
 const Userdb = mongoose.model('userdb', UserSchema);
 
 module.exports = Userdb;
-
-
-
-
-
-
-// const mongoose = require('mongoose');
-
-
-// const CohortSchema = new mongoose.Scheme({
-//     cohortNumber: {type: String, required: true},
-//     residents: {type: Array}
-// });
-
-// const Cohortdb = mongoose.model('cohortdb', CohortSchema);
-
-// module.exports = Cohortdb;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const bcrypt = require('bcrypt');
-// // const { stringify } = require('postcss');
-
-
-// const Schema = mongoose.Schema;
-
-
-// const UserSchema = new Schema({
-//     username: { type: String, required: true },
-//     password: { type: String, required: true },
-//     cohortNumber: { type: String, required: true },
-//     participation: { Number, default: 0 }
-
-// });
-
-// UserSchema.pre('save', async function (next) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//     return next()
-// })
-
-// const User = mongoose.model('User', UserSchema);
-
-// module.exports = User;
